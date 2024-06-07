4c8d761bdcac24eb9538678c79b4bb854d2e4d32
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-06-07 07:57:51 -0500

    feat: chore: :wrench: Se ajustaron las URL

--- js/modules/urls.js
+++ js/modules/urls.js
@@ -1,12 +1,15 @@
 export const local = ({
     albums: "http://172.16.101.146:5802/albums",
     posts: "http://172.16.101.146:5800/posts",
-    users: "http://172.16.101.146:5804/users"
+    users: "http://172.16.101.146:5804/users",
+    comments: "http://172.16.101.146:5801/comments",
+    photos: "http://172.16.101.146:5803/photos"
 });
 
 export const remoto = ({
     albums: "https://53977d67df4867e88ad92ace77f41d81.serveo.net/albums",
     posts: "https://ea9cb12e1fd707dd59cabd0bcfaca5bf.serveo.net/posts",
     users: "https://b426bc73b31fd3e606372972aae17324.serveo.net/users",
-    photos: "https://eeb0d5d633a3ac44a1c6ad64f07d43a4.serveo.net/photos"
+    photos: "https://eeb0d5d633a3ac44a1c6ad64f07d43a4.serveo.net/photos",
+    comments: "https://08c66d6fa43b0d8452d54ee3978c023b.serveo.net/comments"
 });
